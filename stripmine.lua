bac5926673ba732a46b0ca73ce865218ddc81f34
Disk <76448933+Amordisk@users.noreply.github.com>
2023-02-12 19:40:28 -0500

    Update stripmine.lua

--- stripmine.lua
+++ stripmine.lua
@@ -102,8 +102,10 @@ end
 function dig(d,h)
  --if h == 3 then   stops dig from working for some reason
   for i=1,d do
+   veinMine()
    turtle.dig()
    movex(1)
+   veinMine()
    turtle.digUp()
    turtle.digDown()
   end
